4af210c8a9a5309d70fe7f1e14d7a030a623b8a1
ewenosdu29 <ewen.helary@isen-ouest.yncrea.fr>
2025-01-29 11:01:46 +0100

    jd

--- client/client.py
+++ client/client.py
@@ -4,7 +4,7 @@ import requests
 app = Flask(__name__)
 
 # URL de votre API
-API_URL = "http://<i-062dde51c019426ba>"  # Remplacez par l'URL de votre API FastAPI
+API_URL = "http://3.215.16.52"  # Remplacez par l'URL de votre API FastAPI
 
 @app.route("/")
 def index():
