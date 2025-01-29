7a95e9b2e1578fc076466520bf825bcfd30b6e12
ewenosdu29 <ewen.helary@isen-ouest.yncrea.fr>
2025-01-29 11:44:44 +0100

    hbhb

--- client/client.py
+++ client/client.py
@@ -19,4 +19,4 @@ def index():
     return render_template("index.html", etudiants=etudiants)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=True, host="127.0.0.1", port=8080)
